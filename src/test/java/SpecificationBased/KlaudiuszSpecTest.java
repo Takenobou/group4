aa33fe2817ea8b8d4ba6e328a43d594cd366979e
Klaudiusz Halek <kdh10@student.le.ac.uk>
2023-12-08 08:08:12 +0000

    Statement test done

--- src/test/java/SpecificationBased/KlaudiuszSpecTest.java
+++ src/test/java/SpecificationBased/KlaudiuszSpecTest.java
@@ -3,7 +3,7 @@ package SpecificationBased;
 import group4.passwordmanager.model.Credential;
 import group4.passwordmanager.model.CredentialStorage;
 import group4.passwordmanager.service.*;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,6 +12,9 @@ import java.awt.*;
 import java.awt.datatransfer.*;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,7 +29,17 @@ public class KlaudiuszSpecTest {
 
     private CredentialService credentialService;
 
-    // Helper method to simulate user input using Scanner
+    private static final String TEST_FILE_PATH = "test-file.json";
+
+    @AfterAll
+    public static void cleanup() {
+        try {
+            Path testFilePath = Paths.get(TEST_FILE_PATH);
+            Files.deleteIfExists(testFilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     private void simulateUserInput(String input, Consumer<Scanner> test) {
         InputStream inputStream = new ByteArrayInputStream(input.getBytes());
         Scanner scanner = new Scanner(inputStream);
@@ -37,52 +50,33 @@ public class KlaudiuszSpecTest {
             scanner.close();
         }
     }
-
-
-    // Helper method to get the captured output
     @BeforeEach
     public void setup() {
-        // Create a mock CredentialService
         CredentialService credentialService = new CredentialService(new CredentialStorage("test-file.json"));
         List<Credential> mockCredentials = new ArrayList<>();
         mockCredentials.add(new Credential("user1", "password1", "example.com"));
         mockCredentials.add(new Credential("user2", "password2", "example.org"));
         credentialService.addCredential(new Credential("user3", "password3", "example.net"));
-
-        // Set up the SearchService with the mock CredentialService
         searchService = new SearchService(credentialService);
     }
 
     @BeforeEach
     void setUp() {
-        // Create or mock the CredentialService instance for each test
         credentialService = new CredentialService(new CredentialStorage("test-file.json"));
-        // Add some mock credentials for testing
         credentialService.addCredential(new Credential("user1", "password1", "example.com"));
         credentialService.addCredential(new Credential("user2", "password2", "example.org"));
         credentialService.addCredential(new Credential("user3", "password3", "example.net"));
     }
 
-    @AfterEach
-    public void cleanup() {
-        // Reset System.in to its original state after each test
-        System.setIn(System.in);
-    }
-
     @Test
     public void testSearchCredentialsAndPrintDetails_NoMatchingCredentials() {
-        // Create a ByteArrayInputStream to simulate user input
         ByteArrayInputStream in = new ByteArrayInputStream("\n".getBytes()); // Empty input to go back
         System.setIn(in);
 
-        // Create a Scanner with the mocked System.in
         Scanner scanner = new Scanner(System.in);
 
-        // Call the non-static method on the instance of SearchService
         searchService.searchCredentialsAndPrintDetails(scanner, "nonexistent");
 
-        // Assertions based on the expected output or behavior
-        // For simplicity, let's just check if the method runs without exceptions
         assertEquals(1, 1);
     }
     @Test
@@ -91,72 +85,53 @@ public class KlaudiuszSpecTest {
             // Simulate user input "1\nyes\n"
             simulateUserInput("1\nyes\n", scanner -> {
                 searchService.searchCredentialsAndPrintDetails(scanner, "user");
-                // Additional assertions or verifications here
             });
         } catch (Exception e) {
-            // Log or print the exception for debugging purposes
             e.printStackTrace();
             fail("Test failed with an exception: " + e.getMessage());
         }
     }
     @Test
     public void testCopyToClipboard_YesOptionSelected() {
-        // Create a mock Credential
         Credential mockCredential = new Credential("user1", "password1", "example.com");
 
-        // Create a ByteArrayInputStream to simulate user input ("yes")
         ByteArrayInputStream in = new ByteArrayInputStream("yes\n".getBytes());
         System.setIn(in);
 
-        // Create a Scanner with the mocked System.in
         Scanner scanner = new Scanner(System.in);
 
-        // Call the method you want to test
         viewPasswordAndCopyToClipboard(scanner, mockCredential);
 
-        // Assertions based on the expected output or behavior
-        // For simplicity, let's just check if the method runs without exceptions
         assertEquals(1, 1);
     }
     @Test
     public void testCopyToClipboard_NoOptionSelected() {
-        // Create a mock Credential
         Credential mockCredential = new Credential("user2", "password2", "example.org");
 
-        // Create a ByteArrayInputStream to simulate user input ("no")
         ByteArrayInputStream in = new ByteArrayInputStream("no\n".getBytes());
         System.setIn(in);
 
-        // Create a Scanner with the mocked System.in
         Scanner scanner = new Scanner(System.in);
 
-        // Call the method you want to test
         viewPasswordAndCopyToClipboard(scanner, mockCredential);
 
-        // Assertions based on the expected output or behavior
-        // For simplicity, let's just check if the method runs without exceptions
         assertEquals(1, 1);
     }
 
     @Test
     public void testSearchCredentialsAndPrintDetails_InvalidSelectionInput() {
-        // Simulate user input "invalid\n"
         assertThrows(NumberFormatException.class, () -> simulateUserInput("invalid\n", scanner -> {
-            // Call the non-static method on the instance of SearchService
             searchService.searchCredentialsAndPrintDetails(scanner, "user");
         }));
     }
     @Test
     void testInvalidCredentialSelection() {
-        // Prepare test data
         List<Credential> credentials = credentialService.getAllCredentials();
 
-        // Simulate user input "0\n"
         String input = "0\n";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         Scanner scanner = new Scanner(in);
 
-        // Execute the method under test and expect a NumberFormatException
         assertThrows(NumberFormatException.class, () -> {
             int index = Integer.parseInt(scanner.nextLine());
             if (index >= 1 && index <= credentials.size()) {
@@ -166,7 +141,6 @@ public class KlaudiuszSpecTest {
                 throw new NumberFormatException("Invalid selection.");
             }
         });
-        // Verify that the method correctly handles an invalid credential selection
     }
     @Test
     void generateRandomPassword_shouldReturnRandomPassword() {
@@ -176,7 +150,6 @@ public class KlaudiuszSpecTest {
     }
     @Test
     void enterPassword_shouldReturnEnteredOrGeneratedPassword() {
-        // Simulate user input with ByteArrayInputStream
         InputStream input = new ByteArrayInputStream("1\nTestPassword\n2\n".getBytes());
         Scanner scanner = new Scanner(input);
 
@@ -188,7 +161,6 @@ public class KlaudiuszSpecTest {
     }
     @Test
     void enterPassword_shouldReturnDefaultForInvalidOption() {
-        // Simulate user input with ByteArrayInputStream
         InputStream input = new ByteArrayInputStream("invalid\nTestPassword\n".getBytes());
         Scanner scanner = new Scanner(input);
 
@@ -198,7 +170,6 @@ public class KlaudiuszSpecTest {
     @Test
     void editPassword_shouldReturnNewOrGeneratedPassword() {
         String currentPassword = "CurrentPassword";
-        // Simulate user input with ByteArrayInputStream
         InputStream input = new ByteArrayInputStream("1\n1\nNewPassword\n1\n2\n".getBytes());
         Scanner scanner = new Scanner(input);
 
@@ -211,7 +182,6 @@ public class KlaudiuszSpecTest {
     @Test
     void editPassword_shouldReturnCurrentPasswordForInvalidOption() {
         String currentPassword = "CurrentPassword";
-        // Simulate user input with ByteArrayInputStream
         InputStream input = new ByteArrayInputStream("invalid\n".getBytes());
         Scanner scanner = new Scanner(input);
 
@@ -221,7 +191,6 @@ public class KlaudiuszSpecTest {
     @Test
     void editPassword_shouldReturnCurrentPasswordForNoChangeOption() {
         String currentPassword = "CurrentPassword";
-        // Simulate user input with ByteArrayInputStream
         InputStream input = new ByteArrayInputStream("2\n".getBytes());
         Scanner scanner = new Scanner(input);
 
@@ -270,56 +239,40 @@ public class KlaudiuszSpecTest {
     }
     @Test
     void copyToClipboard_shouldCopyTextToClipboard() {
-        // Save the current clipboard contents (if any) to restore it later
         Clipboard systemClipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
         Transferable originalContents = systemClipboard.getContents(null);
-
         try {
             String text = "TestPassword";
             ClipboardService.copyToClipboard(text);
-
-            // Retrieve the content from the system clipboard
             Transferable contents = systemClipboard.getContents(null);
             String copiedText = (String) contents.getTransferData(DataFlavor.stringFlavor);
-
             assertEquals(text, copiedText);
         } catch (UnsupportedFlavorException | IOException e) {
             fail("Unexpected exception: " + e.getMessage());
         } finally {
-            // Restore the original clipboard contents
             systemClipboard.setContents(originalContents, null);
         }
     }
     @Test
     public void testTrackAccessHistory_FirstTimeAccess() {
-        // Arrange
-         // You might need to implement a mock CredentialService
         AccessHistoryTracker accessHistoryTracker = new AccessHistoryTracker(credentialService);
         Credential credential = new Credential();
-
-        // Act
         accessHistoryTracker.trackAccessHistory(credential);
-
-        // Assert
         assertNotNull(credential.getLastAccessed());
-        // You might also want to verify the output to the console using a mocking framework
+
     }
 
     @Test
     public void testTrackAccessHistory_SubsequentAccess() {
-        // Arrange
         AccessHistoryTracker accessHistoryTracker = new AccessHistoryTracker(credentialService);
         Credential credential = new Credential();
         LocalDateTime lastAccessed = LocalDateTime.now().minusDays(1);
         credential.setLastAccessed(lastAccessed);
 
-        // Act
         accessHistoryTracker.trackAccessHistory(credential);
 
-        // Assert
         LocalDateTime currentTimestamp = credential.getLastAccessed();
         assertNotNull(currentTimestamp);
         assertTrue(currentTimestamp.isAfter(lastAccessed));
-        // You might also want to verify the output to the console using a mocking framework
     }
 }
--- src/test/java/StatementBased/KlaudiuszStatementTest.java
+++ src/test/java/StatementBased/KlaudiuszStatementTest.java
@@ -1,4 +1,211 @@
 package StatementBased;
 
+import group4.passwordmanager.model.Credential;
+import group4.passwordmanager.model.CredentialStorage;
+import group4.passwordmanager.service.AccessHistoryTracker;
+import group4.passwordmanager.service.CredentialService;
+import group4.passwordmanager.service.PasswordGenerator;
+import group4.passwordmanager.service.SearchService;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class KlaudiuszStatementTest {
+    private SearchService searchService;
+
+    private AccessHistoryTracker accessHistoryTracker;
+
+    private static final String TEST_FILE_PATH = "test-file.json";
+
+    @AfterAll
+    public static void cleanup() {
+        try {
+            Path testFilePath = Paths.get(TEST_FILE_PATH);
+            Files.deleteIfExists(testFilePath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private void simulateUserInput(String input, Consumer<Scanner> test) {
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        Scanner scanner = new Scanner(inputStream);
+
+        try {
+            test.accept(scanner);
+        } finally {
+            scanner.close();
+        }
+    }
+
+    @BeforeEach
+    public void setup() {
+        CredentialService credentialService = new CredentialService(new CredentialStorage("test-file.json"));
+        List<Credential> mockCredentials = new ArrayList<>();
+        mockCredentials.add(new Credential("user1", "password1", "example.com"));
+        mockCredentials.add(new Credential("user2", "password2", "example.org"));
+        credentialService.addCredential(new Credential("user3", "password3", "example.net"));
+
+        searchService = new SearchService(credentialService);
+        accessHistoryTracker = new AccessHistoryTracker(credentialService);
+    }
+
+    @Test
+    public void testSearchCredentialsAndPrintDetails_NoMatchingCredentials() {
+        // Simulate user input with an empty line
+        simulateUserInput("\n", scanner -> {
+            searchService.searchCredentialsAndPrintDetails(scanner, "nonexistent");
+        });
+    }
+
+    @Test
+    public void testSearchCredentialsAndPrintDetails_MatchingCredentials() {
+        // Simulate user input "1\nyes\n"
+        simulateUserInput("1\nyes\n", scanner -> {
+            searchService.searchCredentialsAndPrintDetails(scanner, "user");
+        });
+    }
+
+    @Test
+    public void testViewPasswordAndCopyToClipboard_CopyPassword() {
+        // Simulate user input "yes\n"
+        simulateUserInput("yes\n", scanner -> {
+            Credential credential = new Credential();
+            SearchService.viewPasswordAndCopyToClipboard(scanner, credential);
+        });
+    }
+
+    @Test
+    public void testViewPasswordAndCopyToClipboard_NoCopy() {
+        // Simulate user input "no\n"
+        simulateUserInput("no\n", scanner -> {
+            Credential credential = new Credential();
+            SearchService.viewPasswordAndCopyToClipboard(scanner, credential);
+        });
+    }
+
+    @Test
+    public void testSearchCredentials_InvalidSelection() {
+        assertThrows(NumberFormatException.class, () -> simulateUserInput("invalid\n", scanner -> {
+            searchService.searchCredentialsAndPrintDetails(scanner, "user");
+        }));
+    }
+    @Test
+    public void testGenerateRandomPassword() {
+        String password = PasswordGenerator.generateRandomPassword();
+        assertEquals(8, password.length());
+    }
+
+    @Test
+    public void testEnterPassword_EnterOwnPassword() {
+        // Simulate user input "1\nuserPassword\n"
+        simulateUserInput("1\nuserPassword\n", scanner -> {
+            String result = PasswordGenerator.enterPassword(scanner);
+            assertEquals("userPassword", result);
+        });
+    }
+
+    @Test
+    public void testEnterPassword_GenerateRandomPassword() {
+        // Simulate user input "2\n"
+        simulateUserInput("2\n", scanner -> {
+            String result = PasswordGenerator.enterPassword(scanner);
+            assertEquals(8, result.length());
+        });
+    }
+
+    @Test
+    public void testEnterPassword_InvalidOption() {
+        // Simulate user input "invalid\nuserPassword\n"
+        simulateUserInput("invalid\nuserPassword\n", scanner -> {
+            String result = PasswordGenerator.enterPassword(scanner);
+            assertEquals("userPassword", result);
+        });
+    }
+
+    @Test
+    public void testEditPassword_ChangePassword_EnterNewPassword() {
+        // Simulate user input "1\n1\nnewPassword\n"
+        simulateUserInput("1\n1\nnewPassword\n", scanner -> {
+            String result = PasswordGenerator.editPassword(scanner, "currentPassword");
+            assertEquals("newPassword", result);
+        });
+    }
+
+    @Test
+    public void testEditPassword_ChangePassword_GenerateNewPassword() {
+        // Simulate user input "1\n2\n"
+        simulateUserInput("1\n2\n", scanner -> {
+            String result = PasswordGenerator.editPassword(scanner, "currentPassword");
+            assertEquals(8, result.length());
+        });
+    }
+
+    @Test
+    public void testEditPassword_ChangePassword_InvalidOption() {
+        // Simulate user input "1\ninvalid\ninvalid\n"
+        simulateUserInput("1\ninvalid\ninvalid\n", scanner -> {
+            String result = PasswordGenerator.editPassword(scanner, "currentPassword");
+            assertEquals("currentPassword", result);
+        });
+    }
+
+    @Test
+    public void testEditPassword_KeepCurrentPassword() {
+        // Simulate user input "2\n"
+        simulateUserInput("2\n", scanner -> {
+            String result = PasswordGenerator.editPassword(scanner, "currentPassword");
+            assertEquals("currentPassword", result);
+        });
+    }
+
+    @Test
+    public void testEditPassword_InvalidChangeOption() {
+        // Simulate user input "invalid\n"
+        simulateUserInput("invalid\n", scanner -> {
+            String result = PasswordGenerator.editPassword(scanner, "currentPassword");
+            assertEquals("currentPassword", result);
+        });
+    }
+    @Test
+    public void testTrackAccessHistory_FirstAccess() {
+        // Simulate user input with an empty line
+        simulateUserInput("\n", inputStream -> {
+            Credential credential = new Credential();
+            accessHistoryTracker.trackAccessHistory(credential);
+        });
+    }
+
+    @Test
+    public void testTrackAccessHistory_SubsequentAccess() {
+        // Simulate user input "1\n"
+        simulateUserInput("1\n", inputStream -> {
+            Credential credential = new Credential();
+            credential.setLastAccessed(LocalDateTime.now().minusDays(1));  // Set a past lastAccessed time
+            accessHistoryTracker.trackAccessHistory(credential);
+        });
+    }
+
+    @Test
+    public void testTrackAccessHistory_NullLastAccessed() {
+        // Simulate user input "1\n"
+        simulateUserInput("1\n", inputStream -> {
+            Credential credential = new Credential();
+            credential.setLastAccessed(null);
+            accessHistoryTracker.trackAccessHistory(credential);
+        });
+    }
 }
