a25def5249f9126ae4240d30b64219f2487ceaa5
hugohull <96350325+hugohull@users.noreply.github.com>
2023-12-08 05:27:52 +0000

    Service message added for deletion of master password

--- src/main/java/group4/passwordmanager/service/MasterService.java
+++ src/main/java/group4/passwordmanager/service/MasterService.java
@@ -2,9 +2,19 @@ package group4.passwordmanager.service;
 
 import group4.passwordmanager.model.Master;
 
+import java.util.Scanner;
+
 public class MasterService {
+
+    private Scanner scanner = new Scanner(System.in);
 
     public void displayMessage(String message) {
         System.out.println(message);
+    }
+
+    public boolean confirmDeletion() {
+        System.out.print("Are you sure you want to delete the Master Password? (yes/no): ");
+        String input = scanner.nextLine();
+        return "yes".equalsIgnoreCase(input.trim());
     }
 }
