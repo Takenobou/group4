181f6b841af5e6470dece57ac34abf4bcedb4e93
hugohull <96350325+hugohull@users.noreply.github.com>
2023-12-08 02:54:35 +0000

    Added Model class for master password

--- src/main/java/group4/passwordmanager/manager/SecurityManager.java
+++ src/main/java/group4/passwordmanager/manager/SecurityManager.java
@@ -1,10 +1,19 @@
 package group4.passwordmanager.manager;
 
 public class SecurityManager {
-}
+
+    private SecurityService securityService;
+
+    public SecurityManager(SecurityService securityService){
+        this.securityService = securityService;
+    }
 
     // Create master pass
+    public void createMaster(SecurityService securityService){
+
+    }
 
     // Edit master pass
 
     // Delete master pass
+}
