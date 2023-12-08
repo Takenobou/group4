868bb088006b1938a3646ac899e841d4613bd3b5
hugohull <96350325+hugohull@users.noreply.github.com>
2023-12-08 03:11:09 +0000

    Renamed SecurityManager to MasterManager

--- src/main/java/group4/passwordmanager/manager/MasterManager.java
+++ src/main/java/group4/passwordmanager/manager/MasterManager.java
@@ -2,16 +2,18 @@ package group4.passwordmanager.manager;
 
 import group4.passwordmanager.service.MasterService;
 
-public class SecurityManager {
+public class MasterManager {
 
     private MasterService masterService;
 
-    public SecurityManager(MasterService masterService){
+    public MasterManager(MasterService masterService){
         this.masterService = masterService;
     }
 
     // Create master pass
     public void createMaster(MasterService masterService){
+
+//        masterService.ge
 
     }
 
