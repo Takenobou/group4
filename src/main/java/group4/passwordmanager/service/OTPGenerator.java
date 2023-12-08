e1d048a0bb6335863bbb65a204a34b5c81de8cf0
hugohull <96350325+hugohull@users.noreply.github.com>
2023-12-08 01:58:46 +0000

    generateOTP method now creates OTP with 1 type of each character

--- src/main/java/group4/passwordmanager/service/OTPGenerator.java
+++ src/main/java/group4/passwordmanager/service/OTPGenerator.java
@@ -1,8 +1,26 @@
 package group4.passwordmanager.service;
 
+import java.security.SecureRandom;
+
 public class OTPGenerator {
 
     public static String generateOTP(){
-        return "OTP";
+
+        // Length and characters of password
+        int length = 14;
+        String alphaCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String numericCharacters = "0123456789";
+        String specialCharacters = "!@#$%^&*";
+        String combinedChars = alphaCharacters + numericCharacters + specialCharacters;
+
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder();
+
+        // Append at least one of each type of character to the password
+        sb.append(alphaCharacters.charAt(random.nextInt(alphaCharacters.length())));
+        sb.append(numericCharacters.charAt(random.nextInt(numericCharacters.length())));
+        sb.append(specialCharacters.charAt(random.nextInt(specialCharacters.length())));
+
+        return sb.toString();
     }
 }
